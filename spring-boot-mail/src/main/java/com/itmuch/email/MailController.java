76849d12cb4d593d459ee068c745ff8f4c02c6ca
itmuch <eacdy0000@126.com>
2019-03-21 17:43:24 +0800

    spring boot发送邮件

--- spring-boot-mail/src/main/java/com/itmuch/email/MailController.java
+++ spring-boot-mail/src/main/java/com/itmuch/email/MailController.java
@@ -1,16 +1,22 @@
 package com.itmuch.email;
 
+import freemarker.template.Configuration;
+import freemarker.template.TemplateException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.mail.MailProperties;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author itmuch.com
@@ -22,6 +28,8 @@ public class MailController {
     private JavaMailSender javaMailSender;
     @Autowired
     private MailProperties mailProperties;
+    @Autowired
+    private Configuration freemarkerConfiguration;
 
     /**
      * 简单邮件测试
@@ -89,7 +97,7 @@ public class MailController {
     }
 
     /**
-     * 内嵌附件的邮件测试
+     * 内联附件的邮件测试
      *
      * @return success
      * @throws MessagingException
@@ -105,6 +113,35 @@ public class MailController {
         // 第二个参数表示是否html，设为true
         messageHelper.setText("<h1>HTML内容..<img src=\"cid:attach\"/></h1>", true);
         messageHelper.addInline("attach", new ClassPathResource("wx.jpg"));
+
+        this.javaMailSender.send(message);
+        return "success";
+    }
+
+    /**
+     * 内联附件的邮件测试
+     *
+     * @return success
+     * @throws MessagingException
+     */
+    @GetMapping("/freemarker")
+    public String freemarker() throws MessagingException, IOException, TemplateException {
+        MimeMessage message = this.javaMailSender.createMimeMessage();
+        // 第二个参数表示是否开启multipart模式
+        MimeMessageHelper messageHelper = new MimeMessageHelper(message, true);
+        messageHelper.setFrom(this.mailProperties.getUsername());
+        messageHelper.setTo("511932633@qq.com");
+        messageHelper.setSubject("基于freemarker模板的邮件测试");
+
+        Map<String, Object> model = new HashMap<>();
+        model.put("username", "itmuch");
+        model.put("event", "IT牧场大事件");
+
+        String content = FreeMarkerTemplateUtils.processTemplateIntoString(
+                this.freemarkerConfiguration.getTemplate("mail.ftl"), model);
+
+        // 第二个参数表示是否html，设为true
+        messageHelper.setText(content, true);
 
         this.javaMailSender.send(message);
         return "success";
